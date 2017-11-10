87b30625547e98b7840e389d7c9912b68839cd8b
taku5151 <taku8017@gmail.com>
2017-11-09 22:01:58 -0800

    Fixed

--- main.js
+++ main.js
@@ -2,7 +2,5 @@ function imageLoaded() {
   console.log("Enhance Picture Loaded!");
 }
 function onLoad() {
-  const img = document.querySelector("img");
-  img.style.opacity = "1.0";
-  img.addEventListener("transitionend", imageLoaded);
+  document.querySelector("img").style.opacity = "1.0";
 }
