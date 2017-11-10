fd061ede801288683522d6aa1ed971e7c468721d
taku5151 <taku8017@gmail.com>
2017-11-09 21:55:49 -0800

    Completed

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function imageLoaded() {
   console.log("Enhance Picture Loaded!");
 }
+function onLoad() {
+  const img = document.querySelector("img");
+  img.style.opacity = "1.0";
+  img.addEventListener("transitionend", imageLoaded);
+}
